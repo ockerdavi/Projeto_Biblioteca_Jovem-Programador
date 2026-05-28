87b3d4c89319dd81e815355ff9a0bd24a04f280d
Davi <davi2009rios@gmail.com>
2026-05-28 20:46:19 -0300

    created LivroService.java class

---
+++ biblioteca/src/main/java/br/com/escola/biblioteca/sevice/LivroService.java
@@ -0,0 +1,48 @@
+ package br.com.escola.biblioteca.sevice;
+
+ import br.com.escola.biblioteca.model.Livro;
+ import br.com.escola.biblioteca.repository.LivroRepository;
+
+ import org.springframework.stereotype.Service;
+
+ import java.util.List;
+
+@Service
+public class LivroService {
+
+    private final LivroRepository repository;
+
+    public LivroService(LivroRepository repository) {
+        this.repository = repository;
+    }
+
+    public Livro salvar(br.com.escola.biblioteca.model.Livro livro) {
+        
+        livro.setQuantidadeDisponivel(
+            livro.getQuantidadeTotal());
+
+            livro.setQuantidadeAlugada(0);
+
+            livro.setQuantidadeRepetida(
+                livro.getQuantidadeTotal());
+            
+        return repository.save(livro);
+    }
+
+    public List<Livro> listaTodos() {
+        return repository.findAll();
+    }
+
+    public Livro buscarPorId(Long id) {
+
+        return repository.findByid(id)
+        .orElseThrow(() ->
+
+            new RuntimeException("Livro não encontrado com id: " + id));
+
+}
+
+public void deletar(Long id) {
+    repository.deleteByid(id);
+}
+}
