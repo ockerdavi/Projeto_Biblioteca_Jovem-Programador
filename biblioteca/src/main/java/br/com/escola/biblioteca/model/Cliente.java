aaa73ef76746d30b757747af6ce06d901b6423a5
alinebatistacampanha07-bit <alinebatistacampanha07@gmail.com>
2026-05-28 20:06:22 -0300

    make cliente.java

--- biblioteca/src/main/java/br/com/escola/biblioteca/model/Cliente.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/model/Cliente.java
@@ -1,3 +1,28 @@
+package br.com.escola.biblioteca.model;
+
+import jakarta.persistence.*; 
+import lombok.*;
+
+@Entity
+@Table(name = "clientes")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Cliente {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY) 
+private Long id;
+
+private String nomeCompleto;
+private String telefone; 
+private String email;
+private String cep;
+private String rua;
+private String numeroCasa;
+private String referencia;
 
 }
