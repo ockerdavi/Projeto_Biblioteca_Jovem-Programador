a144c7938aaf8d94fc79d3e210ce4f1ae1dfa047
Davi <davi2009rios@gmail.com>
2026-05-28 20:06:07 -0300

    criado livro.java

--- biblioteca/src/main/java/br/com/escola/biblioteca/model/Livro.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/model/Livro.java
@@ -1,3 +1,36 @@
+package br.com.escola.biblioteca.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "livros")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Livro {
 
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    @Column(unique = true)
+    private String isbn;
+
+    private String autor;
+
+    private String categoria;
+
+    private Integer quantidadeTotal;
+
+    private Integer quantidadeDisponivel;
+
+    private Integer quantidadeAlugada;
+
+    private Integer quantidadeRepetida;
 }
