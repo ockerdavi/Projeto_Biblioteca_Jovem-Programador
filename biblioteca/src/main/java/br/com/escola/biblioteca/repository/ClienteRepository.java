06242b59408fc421410831d97ad407d7d04e2e5a
Davi Ocker de Oliveira <ockerdavi4@gmail.com>
2026-06-01 20:05:04 -0300

    Commit backend branch on develop branch

--- biblioteca/src/main/java/br/com/escola/biblioteca/repository/ClienteRepository.java
+++ biblioteca/src/main/java/br/com/escola/biblioteca/repository/ClienteRepository.java
@@ -1,3 +1,17 @@
-public class ClienteRepository {
+package br.com.escola.biblioteca.repository;
 
+import br.com.escola.biblioteca.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteRepository 
+    extends JpaRepository<Cliente, Long>{
+    
+    Optional<Cliente> findByEmail(String email);
+    
+    Optional<Cliente> findByCpf(String cpf);
+    
+    List<Cliente> findByAtivoTrue();
+    
 }
